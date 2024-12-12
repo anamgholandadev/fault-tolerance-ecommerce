2c7dc84ba7ac4d966f267997ef699fcfc5a53464
anamgholandadev <ana.mgholandadev@gmail.com>
2024-12-12 20:02:59 -0300

    Acrescentando serviços de fidelity e exchange

--- exchange/src/main/java/com/ufrn/faulttolerance/exchange/controllers/Controller.java
+++ exchange/src/main/java/com/ufrn/faulttolerance/exchange/controllers/Controller.java
@@ -1,22 +1,16 @@
-package com.ufrn.faulttolerance.ecommerce.controllers;
+package com.ufrn.faulttolerance.exchange.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/hello")
 public class Controller {
-    RestClient restClient;
-
-    public Controller() {
-        this.restClient = RestClient.create();
-    }
 
     @GetMapping
     public String helloWorld() {
-        return restClient.get().uri("http://exchange:8080/hello").retrieve().body(String.class);
+        return "Hello From Exchange!";
     }
 
 }
---
+++ exchange/src/test/java/com/ufrn/faulttolerance/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.faulttolerance.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
