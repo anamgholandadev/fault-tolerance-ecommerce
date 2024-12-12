7d66e3df18f09c0bd176674eb9b5800094cc6f84
anamgholandadev <ana.mgholandadev@gmail.com>
2024-12-12 18:48:09 -0300

    Adicionando comunicação entre serviços

---
+++ ecommerce/src/main/java/com/ufrn/faulttolerance/ecommerce/controllers/Controller.java
@@ -0,0 +1,22 @@
+package com.ufrn.faulttolerance.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+    RestClient restClient;
+
+    public Controller() {
+        this.restClient = RestClient.create();
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return restClient.get().uri("http://localhost:8081/hello").retrieve().body(String.class);
+    }
+
+}
