9b885b2e6d4dc7340564801d2b6fc608e7a5aef8
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-12-15 23:31:54 -0300

    Implementa classe de simulação de falhas

--- fidelity/src/main/java/com/ufrn/faulttolerance/fidelity/services/FidelityService.java
+++ fidelity/src/main/java/com/ufrn/faulttolerance/fidelity/services/FidelityService.java
@@ -1,6 +1,7 @@
 package com.ufrn.faulttolerance.fidelity.services;
 
 import com.ufrn.faulttolerance.fidelity.models.dto.BonusRequestDTO;
+import com.ufrn.faulttolerance.fidelity.utils.FailureSimulator;
 import org.springframework.stereotype.Service;
 
 
@@ -11,8 +12,18 @@ import java.util.Map;
 public class FidelityService {
 
     private final Map<String, Integer> userBonuses = new HashMap<>();
+    private final FailureSimulator failureSimulator;
+
+    public FidelityService() {
+        this.failureSimulator = new FailureSimulator(0.1, 30000, 2000);
+    }
 
     public boolean registerBonus(BonusRequestDTO bonusRequestDTO) {
+        if (failureSimulator.shouldTriggerFailure()) {
+            failureSimulator.activateFailure();
+        }
+        failureSimulator.simulateFailureDelay();
+
         userBonuses.put(bonusRequestDTO.getUser(), bonusRequestDTO.getBonus());
         return true;
     }
---
+++ fidelity/src/main/java/com/ufrn/faulttolerance/fidelity/utils/FailureSimulator.java
@@ -0,0 +1,42 @@
+package com.ufrn.faulttolerance.fidelity.utils;
+
+public class FailureSimulator {
+
+    private final double failureProbability;
+    private final int failureDurationMs;
+    private final int delayDurationMs;
+    private boolean isFailureActive = false;
+    private long failureEndTime = 0L;
+
+    public FailureSimulator(double failureProbability, int failureDurationMs, int delayDurationMs) {
+        this.failureProbability = failureProbability;
+        this.failureDurationMs = failureDurationMs;
+        this.delayDurationMs = delayDurationMs;
+    }
+
+    public boolean isFailureActive() {
+        if (isFailureActive && System.currentTimeMillis() > failureEndTime) {
+            isFailureActive = false;
+        }
+        return isFailureActive;
+    }
+
+    public boolean shouldTriggerFailure() {
+        return Math.random() < failureProbability;
+    }
+
+    public void activateFailure() {
+        isFailureActive = true;
+        failureEndTime = System.currentTimeMillis() + failureDurationMs;
+    }
+
+    public void simulateFailureDelay() {
+        if (isFailureActive()) {
+            try {
+                Thread.sleep(delayDurationMs);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
