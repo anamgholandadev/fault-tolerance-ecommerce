6eb7a2798d9ad5164e8e8ef4587e47faf9d85b3c
Diego Filgueiras <diego.filgueiras@smartsuite.imd.ufrn.br>
2025-01-27 16:04:32 -0300

    add stress test script

---
+++ performance_test/stress-test.js
@@ -0,0 +1,22 @@
+import http from "k6/http";
+
+export const options = {
+	executor: "ramping-arrival-rate",
+	stages: [
+		{ duration: "5m", target: 3000 },
+	],
+	thresholds: {
+    http_req_failed: [{ threshold: 'rate<0.15', abortOnFail: true }],
+  },
+};
+
+export default function () {
+  const payload = JSON.stringify({
+    productId: 1,
+    userId: 2,
+    ft: false,
+  });
+  const headers = { "Content-Type": "application/json" };
+  const response = http.post("http://127.0.0.1:8080/ecommerce/buy", payload, {headers});
+}
+
