f0596e69e280a84bf55939a476beb773eef5ec2e
Diego Filgueiras <diego.filgueiras@smartsuite.imd.ufrn.br>
2025-01-27 16:32:05 -0300

    update stress test script

--- performance_test/stress-test.js
+++ performance_test/stress-test.js
@@ -3,7 +3,7 @@ import http from "k6/http";
 export const options = {
 	executor: "ramping-arrival-rate",
 	stages: [
-		{ duration: "5m", target: 3000 },
+		{ duration: "5m", target: 20000 },
 	],
 	thresholds: {
     http_req_failed: [{ threshold: 'rate<0.15', abortOnFail: true }],
