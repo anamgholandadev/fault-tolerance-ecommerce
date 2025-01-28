ad9e64d2f5a7396c68b75760b0cb57d6326b833b
anamgholandadev <ana.mgholandadev@gmail.com>
2025-01-27 23:05:25 -0300

    Adicionando testes de resistencia

---
+++ performance_test/test_without_ft_endurance.js
@@ -0,0 +1,34 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+
+
+export const options = {
+    stages: [
+        { duration: '1m', target: 100 },
+        { duration: '5m', target: 300 },
+        { duration: '1m', target: 0 },
+    ],
+};
+
+export default function () {
+    const payload = JSON.stringify({
+        productId: '1',
+        userId: '2',
+        ft: false
+    });
+
+    const params = {
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    const res = http.post('http://localhost:8080/ecommerce/buy', payload, params);
+    check(res, {
+        'Status is 200': (r) => r.status === 200,
+    });
+}
+export function handleSummary(data) {
+    return {
+        'summary_without_ft.html': htmlReport(data),
+    };
+}
